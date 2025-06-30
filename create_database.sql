c2aa0a8f130299158e1d6b1effd16235aff94f34
Azshira <46813256+JulienL31@users.noreply.github.com>
2025-06-30 08:49:56 +0200

    insert database

---
+++ create_database.sql
@@ -0,0 +1,8 @@
+-- Script de création de la base de données tifosi
+-- Ce script crée la base, l'utilisateur et toutes les tables avec les contraintes
+
+
+DROP DATABASE IF EXISTS tifosi;
+CREATE DATABASE tifosi;
+USE tifosi;
+
